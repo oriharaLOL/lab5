a755c7a519ca85b3af895062faf115dc749e1f7a
oriharaLOL <orihara2012@mail.ru>
2016-06-11 01:06:23 +0600

    попытка слияния

---
+++ file/diskr.h
@@ -0,0 +1,6 @@
+#ifndef DIS_H
+#define DIS_H
+
+void dis(float a,float b,float c);
+
+#endif
---
+++ file/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+float a,b,c;
+printf("input a,b,c:");
+scanf("%f%f%f",&a,&b,&c);
+dis(a,b,c);
+}
